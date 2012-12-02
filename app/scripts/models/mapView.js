fcb97c2dfc94fb65348d5f9ebd3426ebfe2134eb
Lucas Ray <lucas@lucasray.com>
2012-12-02 15:50:12 -0500

    added vector "Gs"

--- app/scripts/models/mapView.js
+++ app/scripts/models/mapView.js
@@ -4,23 +4,20 @@
  *  @author Kenneth Murphy (kmmurphy)
  */
 
-
-
 function mapInit(data){
-	console.log("changing page");
-	$.mobile.changePage($("#mapView"), { transition: "slideup"} );
-	
-	$("#mapView .contentBubble").css("height", "80%");
-	$("#map_canvas").css("height", "100%");
-	$("#map_canvas").css("width", "100%");
-	
-	
-	var mapOptions = {
-		center: new google.maps.LatLng(data.lat, data.long),
-		zoom: 15,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
+  console.log("changing page");
+  $.mobile.changePage($("#mapView"), { transition: "slideup"} );
 
-	//center = new google.maps.LatLng(data.lat, dat);
-	var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+  $("#mapView .contentBubble").css("height", "80%");
+  $("#map_canvas").css("height", "100%");
+  $("#map_canvas").css("width", "100%");
+
+  var mapOptions = {
+    center: new google.maps.LatLng(data.lat, data.long),
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  //center = new google.maps.LatLng(data.lat, dat);
+  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 }

ab396022b06e73bda2e7d9014b83273017babd88
Nick LaGrow <nlagrow@andrew.cmu.edu>
2012-12-04 16:27:51 -0500

    test heroku

--- http_request_server.js
+++ http_request_server.js
@@ -203,7 +203,7 @@
       passport.use(new FacebookStrategy({
         clientID: "121594388000133",
         clientSecret: "0d478582454ff9d8755f2ebb48dccf28",
-        callbackURL: "http://localhost:8080"
+        callbackURL: "http://" + window.location.hostname
       }, function(accessToken, refreshToken, profile, done) {
         return User.findOrCreate.apply(User, __slice.call(unused).concat([function(err, user) {
           if (err) {

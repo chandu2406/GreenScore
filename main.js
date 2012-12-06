70d250d28b1f62f737cf7b6a4aef1331cde4d3f0
Nick LaGrow <nlagrow@andrew.cmu.edu>
2012-12-06 12:06:35 -0500

    heroku test

--- main.js
+++ main.js
@@ -20,8 +20,9 @@
       @brief Main function.
     */
 
-    var http_server;
+    var http_server, socket_server;
     http_server = new HTTPRequestServer(process.env.PORT || 8080);
+    socket_server = new SocketServer(3000);
     http_server.listen();
     return socket_server.listen();
   };

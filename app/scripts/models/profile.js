f2578afb91e93baa3a9778102a9f4e93ebef4759
Kenneth Murphy <kmmurphy@andrew.cmu.edu>
2012-12-07 18:54:51 -0500

    adding profile

---
+++ app/scripts/models/profile.js
@@ -0,0 +1,15 @@
+/** @file profile.js
+ *  @brief model of a user's profile
+ *
+ *  @author Kenneth Murphy (kmmurphy)
+ */
+
+
+
+/*
+  going to include general zillow info(updated info)
+
+
+
+
+*/
